6b21c94d47199a12f79c500e5806eae71cb7893f
Gilmar Filho <gilmarfilho1985@gmail.com>
2019-04-06 17:20:36 -0300

    Atualizacao

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<head></head>
+<head>Aula bit</head>
 <body>
     <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ut tempore officia. Asperiores aspernatur cum perferendis omnis eligendi voluptates quasi explicabo, eius minus. Exercitationem, explicabo repellat illum fuga cupiditate suscipit.
